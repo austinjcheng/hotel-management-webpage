899e5b921a92cd34a34d195d906bc0961f7af197
patrickly <patrickkimly@gmail.com>
2017-10-28 07:19:50 -0700

    working on register or signup

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,18 @@ const passport = require('passport');
 // Bring in User Model
 let User = require('../models/user');
 
+// Register form
+router.get('/register', function(req, res){
+  res.render('register');
+});
+
 // Login Form
 router.get('/login', function(req, res){
 //  res.send('heya login');
   res.render('login');
 });
+
+
+
 
 module.exports = router;
