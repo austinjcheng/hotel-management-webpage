e565eda76f8fd08d68674f15ac37a9cbdbf251f2
patrickly <patrickkimly@gmail.com>
2017-10-28 07:25:33 -0700

    register sign up works with the database post request

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,67 @@ router.get('/register', function(req, res){
   res.render('register');
 });
 
+// Register process
+router.post('/register', function(req, res){
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('register', {
+      errors: errors
+    });
+  }  else {
+    let newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password
+    });
+
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hashedPassword){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hashedPassword;
+
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success', 'You are now registered and can log in');
+            res.redirect('/users/login');
+          }
+        })
+      });
+    });
+  }
+});
+
+
+
+
 // Login Form
 router.get('/login', function(req, res){
 //  res.send('heya login');
   res.render('login');
 });
+
+
 
 
 
