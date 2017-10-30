9aa9602901377dd0a129a67e2167eb7b864afbb9
patrickly <patrickkimly@gmail.com>
2017-10-29 20:59:35 -0700

    can run guest on port 3002, manager on port 3001

--- manager/app.js
+++ manager/app.js
@@ -142,6 +142,6 @@ let users = require('./routes/users');
 app.use('/users', users);
 
 // Start server
-app.listen(3000, function(){
-  console.log('server started on port 3000');
+app.listen(3001, function(){
+  console.log('server started on port 3001');
 });
