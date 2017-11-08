41435313924ae36044f0c8afc9c19aca78430226
patrickly <patrickkimly@gmail.com>
2017-11-08 12:13:19 -0800

    if guest enters dates that overlaps with already existing reservations, display none

--- guest/models/loom.js
+++ guest/models/loom.js
@@ -6,12 +6,10 @@ var Schema = mongoose.Schema;
 var LoomSchema = mongoose.Schema ({
     room_number: Number,
     roomstyle: String,
-    reserved: [
-        {
-            from: String,
-            to: String
-        }
-    ]
+    reserved: {
+      from: String,
+      to: String
+    }
 });
 
 let Loom = module.exports = mongoose.model('Loom', LoomSchema);
